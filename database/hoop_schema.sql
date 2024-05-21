81f6ef6b875a1bce4d8da741edee338b30201fdb
DJRamsay <125953642+DJRamsay@users.noreply.github.com>
2024-05-21 12:02:33 +0200

    Update hoop_schema.sql
    Corrected name, added constraints and max_profiles to subscription

--- database/hoop_schema.sql
+++ database/hoop_schema.sql
@@ -1,35 +1,36 @@
 --Creates the schema for the Hoop database
 
 -- Recreate the database if it already exists
-DROP DATABASE IF EXISTS COS210_HOOP;
+DROP DATABASE IF EXISTS COS221_HOOP;
 
 -- Create Hoop database
-CREATE DATABASE COS210_HOOP;
+CREATE DATABASE COS221_HOOP;
 
 -- Use Hoop database
-USE COS210_HOOP;
+USE COS221_HOOP;
 
 -- Create Tables and Relationships...
 
 -- Create subscription table
 CREATE TABLE IF NOT EXISTS subscription (
     subscription_id INT AUTO_INCREMENT PRIMARY KEY,
-    subscription_type ENUM('basic', 'standard', 'premium'),
-    subscription_rate DECIMAL(10, 2),
-    subscription_start DATE
-    max_profiles INT;
+    subscription_type ENUM('basic', 'standard', 'premium') NOT NULL,
+    subscription_rate DECIMAL(10, 2) NOT NULL,
+    subscription_start DATE NOT NULL,
+    max_profiles INT NOT NULL
 );
 
 -- Create account table
 CREATE TABLE IF NOT EXISTS account (
     account_id INT AUTO_INCREMENT PRIMARY KEY,
-    subscription_id INT,
-    fname VARCHAR(50),
-    sname VARCHAR(50),
-    phone VARCHAR(20),
-    email VARCHAR(100),
-    password VARCHAR(100),
-    notif_pref BOOLEAN,
+    subscription_id INT NOT NULL,
+    fname VARCHAR(50) NOT NULL,
+    sname VARCHAR(50) NOT NULL,
+    phone VARCHAR(20) NOT NULL,
+    email VARCHAR(100) NOT NULL,
+    account_start DATETIME NOT NULL,
+    password VARCHAR(100) NOT NULL,
+    notif_pref BOOLEAN NOT NULL DEFAULT TRUE,
 
     FOREIGN KEY (subscription_id) REFERENCES subscription(subscription_id)
 );
@@ -37,8 +38,8 @@ CREATE TABLE IF NOT EXISTS account (
 -- Create profile table
 CREATE TABLE IF NOT EXISTS profile (
     profile_id INT AUTO_INCREMENT PRIMARY KEY,
-    account_id INT,
-    profile_age INT,
+    account_id INT NOT NULL,
+    profile_age INT NOT NULL,
     profile_icon BLOB,
 
     FOREIGN KEY (account_id) REFERENCES account(account_id)
@@ -47,8 +48,8 @@ CREATE TABLE IF NOT EXISTS profile (
 -- Create title table
 CREATE TABLE IF NOT EXISTS title (
     title_id INT AUTO_INCREMENT PRIMARY KEY,
-    title_name VARCHAR(255),
-    title_type ENUM('MOVIE', 'SHOW'),
+    title_name VARCHAR(255) NOT NULL,
+    title_type ENUM('MOVIE', 'SHOW') NOT NULL,
     release_date DATE,
     image BLOB,
     genre VARCHAR(255),
@@ -57,22 +58,24 @@ CREATE TABLE IF NOT EXISTS title (
     pg_rating VARCHAR(10),
     rating FLOAT,
     language VARCHAR(50),
-    fss_address VARCHAR(255)
+    fss_address VARCHAR(255) NOT NULL DEFAULT "https://www.fss_address.com" 
 );
 
 -- Create series table
 CREATE TABLE IF NOT EXISTS series (
-     title_id INT PRIMARY KEY,
-    season_num INT,
-    episode_num INT,
+	series_id INT AUTO_INCREMENT PRIMARY KEY,
+    title_id INT NOT NULL,
+    season_num INT NOT NULL,
+    episode_num INT NOT NULL,
 
     FOREIGN KEY (title_id) REFERENCES title(title_id)
 );
 
 -- Create movie table
 CREATE TABLE IF NOT EXISTS movie (
-    title_id INT PRIMARY KEY,
-    length INT,
+	movie_id INT AUTO_INCREMENT PRIMARY KEY,
+    title_id INT NOT NULL,
+    length INT NOT NULL,
 
     FOREIGN KEY (title_id) REFERENCES title(title_id)
 );
@@ -80,18 +83,16 @@ CREATE TABLE IF NOT EXISTS movie (
 -- Create credit table
 CREATE TABLE IF NOT EXISTS credits (
     credit_id INT AUTO_INCREMENT PRIMARY KEY,
-    fname VARCHAR(50), 
-    sname VARCHAR(50),
-    biography TEXT,
-    DOB DATE
+    fname VARCHAR(50) NOT NULL, 
+    sname VARCHAR(50) NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS title_credits (
     title_credit_id INT AUTO_INCREMENT PRIMARY KEY,
-    title_id INT,
-    credit_id INT,
+    title_id INT NOT NULL,
+    credit_id INT NOT NULL,
     role VARCHAR(100),
-    credit_type ENUM('cast', 'crew'),
+    credit_type ENUM('ACTOR', 'DIRECTOR'),
 
     FOREIGN KEY (title_id) REFERENCES title(title_id),
     FOREIGN KEY (credit_id) REFERENCES credits(credit_id)
@@ -101,26 +102,27 @@ CREATE TABLE IF NOT EXISTS title_credits (
 -- Create review table
 CREATE TABLE IF NOT EXISTS review (
     review_id INT AUTO_INCREMENT PRIMARY KEY,
-    title_id INT,
-    profile_id INT,
-    review TEXT,
-    rating INT, 
-    timestamp DATETIME,
+    title_id INT NOT NULL,
+    profile_id INT NOT NULL,
+    review TEXT NOT NULL,
+    rating INT NOT NULL, 
+    timestamp DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
 
     FOREIGN KEY (title_id) REFERENCES title(title_id),
     FOREIGN KEY (profile_id) REFERENCES profile(profile_id)
 );
 
 -- Create preferances table
-CREATE TABLE IF NOT EXISTS preferances (
+CREATE TABLE IF NOT EXISTS preferences (
     pref_id INT AUTO_INCREMENT PRIMARY KEY,
-    title_id INT,
-    profile_id INT,
+    title_id INT NOT NULL,
+    profile_id INT NOT NULL,
 
     FOREIGN KEY (title_id) REFERENCES title(title_id),
     FOREIGN KEY (profile_id) REFERENCES profile(profile_id)
 );
 
+--Trigger to ensure max_profiles is not violated
 DELIMITER //
 
 CREATE TRIGGER before_profile_insert BEFORE INSERT ON profile
